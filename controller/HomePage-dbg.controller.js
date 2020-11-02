bb9e49abe4acde584f9bb13db2e1aaf710cf907e
Sylvain Michel <sylmic@arvato-services.pl>
2020-11-02 14:31:05 +0100

    adding background to tile

--- controller/HomePage-dbg.controller.js
+++ controller/HomePage-dbg.controller.js
@@ -14,48 +14,18 @@ sap.ui.define([
 
 		},
 
-		
-		navToNext: function (oEvent) {
-			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			var sLabel = oEvent.getSource().getAriaLabel();
-			switch(sLabel){
-				case "envrionment":
-					oRouter.navTo("environementLaunch");
-					break;
-				case "tech":
-					oRouter.navTo("techLaunch");
-					break;
-				case "transhumanism":
-					oRouter.navTo("transhumanismLaunch");
-					break;
-				case "travel":
-					oRouter.navTo("travelLaunch");
-					break;
-				case "sport":
-					oRouter.navTo("sportLaunch");
-					break;
-				case "videoGame":
-					oRouter.navTo("videoGameLaunch");
-					break;
-				case "boardGame":
-					oRouter.navTo("boardGameLaunch");
-					break;
-				case "personalDevelopment":
-					oRouter.navTo("personalDevelopmentLaunch");
-					break;
-				case "aboutMe":
-					oRouter.navTo("aboutMe");
-					break;
+		onAboutMe: function () {
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.navTo("aboutMe");
 			}
-		}
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf web.app.theCuriousDude.view.HomePage
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+			/**
+			 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+			 * (NOT before the first rendering! onInit() is used for that one!).
+			 * @memberOf web.app.theCuriousDude.view.HomePage
+			 */
+			//	onBeforeRendering: function() {
+			//
+			//	},
 
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
