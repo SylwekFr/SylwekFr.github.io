60fc94f7dfe11bf121c6efc67b532d77772d29c8
Sylvain Michel <sylvain.michel@2018.icam.fr>
2021-11-17 21:40:51 +0100

    adding dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,55 @@ import NotFound from './pages/not-found';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
 
-const theme = createTheme();
+export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
 enum routes {
   home = "/",
   book = "/books",
   tabletopGame = "/tabletop-game",
   aboutMe = "/about-me"
 }
+
+
+
 function App(): JSX.Element {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline /> 
-        <Header title="The curious dude"/>
-         <BrowserRouter>
-          <Switch>
-            <Route path={routes.home} exact component={Home} />
-            <Route path={routes.book} exact component={Books} />
-            <Route path={routes.tabletopGame} exact component={TabletopGames} />
-            <Route path={routes.aboutMe} exact component={AboutMe} />
-            <Route component={NotFound} />
-          </Switch>
-         </BrowserRouter>
-          
-    </ThemeProvider>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> 
+          <Header title="The curious dude"/>
+          <BrowserRouter>
+            <Switch>
+              <Route path={routes.home} exact component={Home} />
+              <Route path={routes.book} exact component={Books} />
+              <Route path={routes.tabletopGame} exact component={TabletopGames} />
+              <Route path={routes.aboutMe} exact component={AboutMe} />
+              <Route component={NotFound} />
+            </Switch>
+          </BrowserRouter>        
+      </ThemeProvider>
+    </ColorModeContext.Provider>
     
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,16 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
-import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import SettingsIcon from '@mui/icons-material/Settings';
+import React, { useState } from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { useTheme } from '@mui/material/styles';
+import { useContext } from 'react';
+import { ColorModeContext } from '../App';
 
 interface HeaderProps {
     title: string;
@@ -7,6 +18,23 @@ interface HeaderProps {
 
 export default function Header(props: HeaderProps){
     const { title } = props;
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const colorMode = useContext(ColorModeContext);
+    const theme = useTheme();
+
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const onChangeMode = () => {
+        setAnchorEl(null);
+        colorMode.toggleColorMode();
+    }
+
     return(
         <>
             <AppBar position="sticky" sx={{mb: 3}}>
@@ -20,6 +48,40 @@ export default function Header(props: HeaderProps){
                       sx={{ flex: 1 }}>
                           {title}
                       </Typography>
+                    <div>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleMenu}
+                            color="inherit"
+                            >
+                            <SettingsIcon/>
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                            }}
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                            >
+                            <MenuItem onClick={onChangeMode}>
+                                <ListItemIcon>
+                                    {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+                                </ListItemIcon>
+                                <ListItemText>{theme.palette.mode} mode</ListItemText>
+                            </MenuItem>
+                        </Menu>
+                    </div>
                 </Toolbar>
             </AppBar>
         </>
