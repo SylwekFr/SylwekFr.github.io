e51a219d1bac9497d682476939ed7f3aeb5640c5
Sylvain Michel <sylvain.michel@2018.icam.fr>
2021-11-17 14:54:31 +0100

    adding some hobbies

--- src/data/hobbies.tsx
+++ src/data/hobbies.tsx
@@ -2,6 +2,13 @@ import { Hobby } from '../types'
 
 export const hobbies: Hobby[] = [
     {
+        abbreviation: "MD",
+        federationLogo: "../img/diego-logo.jpg",
+        name: "My Dog",
+        picture: "../img/diego-pics.jpg",
+        sumUp: "Maybe my biggest hobby is spending time with my dog, going on walk and learning him new tricks or just chilling"
+    },
+    {
         abbreviation: "RD",
         federationLogo: "https://www.iconpacks.net/icons/2/free-opened-book-icon-3169-thumb.png",
         name: "Reading",
@@ -14,5 +21,26 @@ export const hobbies: Hobby[] = [
         name: "Sambo",
         picture: "https://sambo.sport/upload/iblock/aff/aff35819b812ebae85383bfaf5bd8c1b.jpg",
         sumUp: "Sambo is a russian originated martial art I love to practice. With practice, will, the right spiritual way & truth (in ourself and other practicing with us) it lead to improve self-realization, power & drive it also carry the values of freedom, friendship, respect & inculsion"
+    },
+    {
+        abbreviation: "TG",
+        federationLogo: "https://i.pinimg.com/originals/d9/3d/91/d93d91a57281e6e96dc86c97c5047b30.png",
+        name: "Tabletop Games",
+        picture: "https://media.wired.com/photos/60c000d6206ef134c14e7571/master/pass/games_board-games_2FR3A46.jpg",
+        sumUp: "What a pleasing time to meet with friend around a tabletop game, enjoing a drink and discution or going roleplay"
+    },
+    {
+        abbreviation: "TR",
+        federationLogo: "https://www.creativefabrica.com/wp-content/uploads/2020/02/10/Travel-Logo-Graphics-1-36.jpg",
+        name: "Travel",
+        picture: "https://poradniksukces.com/wp/wp-content/uploads/2016/05/Travel-suitcase.jpg",
+        sumUp: "I enjoy to travel and get to knows new cultures, I had the chance to visit Albania, Andorra, Austria, Belgium, England, France, Hungary, Iceland, Italy, Netherlands, Poland, Slovakia, Spain"
+    },
+    {
+        abbreviation: "VG",
+        federationLogo: "https://i.pinimg.com/originals/72/3d/0a/723d0af616b1fe7d5c7e56a3532be3cd.png",
+        name: "Video Games",
+        picture: "https://i.pinimg.com/736x/b7/6b/82/b76b82fed49d43f0d2e2555b7ced5a91--game-concept-art-fallout-.jpg",
+        sumUp: "Having a hard day ? let's go to a virtual world where you're the hero (or the villain 😉) to change mind, I am personally a fan of the Fallout series and universe"
     }
 ]
