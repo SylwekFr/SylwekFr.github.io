925f38bd80858789fda01cae7751bb194c733423
Sylvain Michel <47174755+SylwekFr@users.noreply.github.com>
2023-05-23 07:06:12 +0000

    fix: :bug: fix name duplication

--- src/aboutMe/components/skill-container.tsx
+++ src/aboutMe/components/skill-container.tsx
@@ -5,10 +5,10 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { FC, useState } from 'react';
-import { SkillContainer } from '../entities/skill';
+import { SkillContainer as SkillContainr } from '../entities/skill';
 import SkillItem from './skill-item';
 
-const SkillContainer: FC<SkillContainer> = (props: SkillContainer) => {
+const SkillContainer: FC<SkillContainr> = (props: SkillContainr) => {
   const { data, icon, name } = props;
   const [open, setOpen] = useState(false);
 
